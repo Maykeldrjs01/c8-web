a2ab293c58d78aed7550b652b2a184080e2a385b
beemerproj <iamjhonelmermagloyuan@gmail.com>
2022-05-05 15:21:12 +0800

    Add subscribers page and assets

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard.index')" :active="request()->routeIs('dashboard.*')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('subscribers.index')" :active="request()->routeIs('subscribers.*')">
+                        {{ __('Subscribers') }}
+                    </x-nav-link>
                 </div>
             </div>
 
