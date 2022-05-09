299fdd658aa90b88d634c7ed05187aa99fdffdb2
beemerproj <iamjhonelmermagloyuan@gmail.com>
2022-05-09 22:41:01 +0800

    Redirect edit button to subscriber index form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,12 +13,12 @@
                 <select name="group">
                     @foreach($groups as $option)
                     <option value="{{ $option->GROUP_ID }}" 
-                    @isset ( $filter )
-                        @if($option->GROUP_ID == $filter)
-                            selected="true"
-                        @endif
-                    @endisset
-                    >{{ $option->GROUP_ID }}</option>
+                        @isset ( $filter ) 
+                            @if($option->GROUP_ID == $filter)
+                                selected="true"
+                            @endif
+                        @endisset
+                        >{{ $option->GROUP_ID }}</option>
                     @endforeach
                 </select>
             </form>
@@ -61,11 +61,17 @@
                                 </td>
                                 <td class="py-3 px-6 text-center">
                                     <div class="flex item-center justify-center">
-                                        <div class="w-5 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                            </svg>
-                                        </div>
+                                        <form method="post" action="{{ route('subscribers.update', ['group'=>$subscriber->GROUP_ID, 'name'=>$subscriber->NAME]) }}">
+                                            @method('PUT')
+                                            @csrf
+                                            <div class="w-5 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                <button type="submit" value=""> Edit
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        </form>
                                         <div class="w-5 mr-2 transform hover:text-purple-500 hover:scale-110">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
