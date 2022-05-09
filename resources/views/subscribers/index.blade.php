9fc56caf10f8c5694f30bcfb3ede77f6666d7b96
beemerproj <iamjhonelmermagloyuan@gmail.com>
2022-05-09 22:42:13 +0800

    Still trying to fetch old data for updates

--- resources/views/subscribers/index.blade.php
+++ resources/views/subscribers/index.blade.php
@@ -13,7 +13,9 @@
                     <!-- Row -->
                     <div class="w-full xl:w-4/5 lg:w-12/12 grid h-full place-items-center">
                         <!-- Col -->
+
                         <!-- <div id="avatar-container" class="w-full h-auto bg-gray-400 hidden lg:block lg:w-1/2 bg-cover rounded-l-lg" style="background-image: url('https://avatars.dicebear.com/api/adventurer-neutral/calibr8.svg')"></div> -->
+
                         <!-- Col -->
                         <div class="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none">
 
@@ -26,13 +28,13 @@
                                         Fullname
                                     </label>
 
-                                    <x-input id="name" class="block mt-1 w-full leading-tight text-gray-700" type="text" name="name" :value="old('name')" placeholder="John Doe" onkeypress="changeAvatar(this)" autofocus/>
+                                    <x-input id="name" class="block mt-1 w-full leading-tight text-gray-700" type="text" name="name" :value='""' placeholder="John Doe" autofocus />
                                 </div>
                                 <div class="mb-6">
                                     <label class="block mb-2 font-bold text-gray-700" for="number">
                                         Phone Number
                                     </label>
-                                    <x-input id="number" class="block mt-1 w-full leading-tight text-gray-700" type="text" name="number" :value="old('number')" placeholder="09xxxxxxxxx" required/>
+                                    <x-input id="number" class="block mt-1 w-full leading-tight text-gray-700" type="text" name="number" :value='""' placeholder="09xxxxxxxxx" required />
                                 </div>
                                 <div class="mb-6">
                                     <label class="block mb-2 font-bold text-gray-700" for="group">
