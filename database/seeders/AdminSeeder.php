5266a5f0f7571e332bde804b874bb65ca06427fe
beemerproj <iamjhonelmermagloyuan@gmail.com>
2022-05-11 09:45:35 +0800

    Add db seeder for admin

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds for admin.
+     *
+     * Change this however you want
+     * 
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'username' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'is_admin' => '1',
+        ]);
+    }
+}
