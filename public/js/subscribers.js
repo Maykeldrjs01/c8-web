74ea0dad292ec05ef4935f9fef3a915e81207381
beemerproj <iamjhonelmermagloyuan@gmail.com>
2022-05-19 00:03:55 +0800

    Add separate scripts for subscribers page

--- public/js/subscribers.js
+++ public/js/subscribers.js
@@ -5,3 +5,33 @@ function changeAvatar(input){
 
     avatarContainer.style.backgroundImage = "url('https://avatars.dicebear.com/api/adventurer-neutral/"+name+".svg')";
 }
+
+
+// const modal = document.querySelector("#modal");
+// const modalOverlay = document.querySelector("#modal-overlay");
+// const openButton = document.querySelector("#open-button");
+// const closeButton = document.querySelector("#close-button");
+
+// openButton.addEventListener("click", () => {
+//   modal.classList.toggle("closed");
+//   modalOverlay.classList.toggle("closed");
+// });
+
+// function openModal(){
+//   modal.classList.toggle("closed");
+//   modalOverlay.classList.toggle("closed");
+//   $.ajax({
+//     url: "/dashboard/filtered",
+//     type: "POST"
+//   })
+// }
+
+// function closeModal(){
+//   modal.classList.toggle("closed");
+//   modalOverlay.classList.toggle("closed");
+// }
+
+// closeButton.addEventListener("click", () => {
+//   modal.classList.toggle("closed");
+//   modalOverlay.classList.toggle("closed");
+// });
